a9c107ff560aae6a22284afc9d72e9b8e213602e
Godinhoweverson <>
2023-10-09 21:13:32 +0000

    Added Recipe model with Cloudinary integration for images.
    - Defined a new Recipe model with fields for title, content, creation date, and likes.
    - Integrated CloudinaryField for handling recipe images.
    - Added utility method to fetch the number of likes for a recipe.

--- cook_recipe/models.py
+++ cook_recipe/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Recipe(models.Model):
+    featured_image = CloudinaryField('image')
+    title = models.CharField(max_length=150, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    likes = models.ManyToManyField(User, related_name='recipes_likes', blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def number_of_likes(self):
+        return self.likes.count()
