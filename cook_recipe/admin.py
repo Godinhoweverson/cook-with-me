7aeac3a3b103e62c832b374207e6baaf476b1f43
Godinhoweverson <>
2023-10-11 10:53:50 +0000

    Added in admin.py list_display, list_filter, and search_fields

--- cook_recipe/admin.py
+++ cook_recipe/admin.py
@@ -5,6 +5,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'created_date')
+    list_filter = ('title', 'created_date')
+    search_fields = ['title', 'created_date']
     summernote_fields = ('content')
 
 
