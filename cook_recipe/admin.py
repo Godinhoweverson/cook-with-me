671cb159fb1802d0e3171cfc03cbaebdcdd3efcf
Godinhoweverson <>
2023-10-11 12:36:40 +0000

    Added admin configurations for Comment model, Aproved comments is done

--- cook_recipe/admin.py
+++ cook_recipe/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
@@ -10,5 +10,15 @@ class RecipeAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'created_date']
     summernote_fields = ('content')
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('recipe', 'user', 'first_name', 'created_on', 'content_body', 'comment_approved')
+    list_filter = ('recipe', 'user', 'created_on', 'email')
+    search_fields = ['recipe', 'user', 'created_on']
+    actions =['comment_approved']
+
+    def comment_approved(self, request, queryset):
+        queryset.update(approved=True)
 
 # Register your models here.
