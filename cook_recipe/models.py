2001b2d56aa966a73e16f9ee6e58c6ed2159eaf9
Godinhoweverson <>
2023-10-11 13:56:07 +0000

    Added 'status' field to Recipe model with choices and default value

--- cook_recipe/models.py
+++ cook_recipe/models.py
@@ -3,11 +3,15 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
    
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
 class Recipe(models.Model):
     featured_image = CloudinaryField('image')
     title = models.CharField(max_length=150, unique=True)
     created_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='recipes_likes', blank=True)
 
     def __str__(self):
