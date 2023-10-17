c8807add7ab01e4e6ba6a7b517ea77497eabda08
Godinhoweverson <>
2023-10-17 11:40:50 +0000

    Add tests for Comment model. - Ensure proper creation of comments. - Test the string representation of comments. _ Handle comments without user data but with name and email.

--- cook_recipe/tests/test_models.py
+++ cook_recipe/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from cook_recipe.models import Recipe
+from cook_recipe.models import Recipe, Comment
 
 class RecipeModelTest(TestCase):
 
@@ -27,3 +27,41 @@ class RecipeModelTest(TestCase):
         self.assertEqual(self.recipe.number_of_likes(), 1)
         self.recipe.likes.add(self.user2)
         self.assertEqual(self.recipe.number_of_likes(), 2)
+
+
+class CommentModelTest(TestCase):
+
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username = 'testuser',
+            password = 'testpass123'
+        )
+
+        self.recipe = Recipe.objects.create(
+            title = 'Test Recipe'
+        )
+
+        self.comment = Comment.objects.create(
+            recipe = self.recipe,
+            user = self.user,
+            content_body = 'This is a test comment.',
+        )
+
+    def test_comment_creation(self):
+        self.assertIsInstance(self.comment, Comment)
+        self.assertEqual(self.comment.content_body, 'This is a test comment.')
+
+    def test_comment_str(self):
+        self.assertEqual(str(self.comment), 'Comment This is a test comment. by testuser')
+
+    def test_comment_without_user(self):
+        comment_without_user = Comment.objects.create(
+            recipe = self.recipe,
+            first_name = "John",
+            last_name = "Doe",
+            email = "john.doe@example.com",
+            content_body =  "Another test comment."
+        )
+        self.assertEqual(str(comment_without_user), 'Comment Another test comment. by John')
+
