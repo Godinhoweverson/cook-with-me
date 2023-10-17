6b55f9801939ddd12b94fc48640aedda6a126f3b
Godinhoweverson <>
2023-10-17 09:48:41 +0000

    Fixed indentation and syntax errors in test_models.py

--- cook_recipe/tests/test_models.py
+++ cook_recipe/tests/test_models.py
@@ -1,48 +1,29 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from cook_recipe.models import Recipe, Comment
+from cook_recipe.models import Recipe
 
 class RecipeModelTest(TestCase):
 
-
-    def setup(self):
-        # create a user fo test
+    def setUp(self):  
         self.user1 = User.objects.create_user(username='testuser1', password='testpass123')
         self.user2 = User.objects.create_user(username='testuser2', password='testpass456')
 
-        # create a recipe for test
+        
         self.recipe = Recipe.objects.create(
-            title = 'Test Recipe',
-            content = 'This is a test content for the recipe.'
+            title='Test Recipe',
+            content='This is a test content for the recipe.'
         )
-
 
     def test_recipe_creation(self):
-        #Checked if the recipe was created correctly 
         self.assertEqual(self.recipe.title, 'Test Recipe')
-        self.assertEqual(self.recipe.content, 'This is test to verify if the content for recipe was created'
-        
-        )
+        self.assertEqual(self.recipe.content, 'This is a test content for the recipe.') 
 
-    
     def test_recipe_str_representation(self):
-        #verify the string representation
         self.assertEqual(str(self.recipe), 'Test Recipe')
 
-
     def test_number_of_likes(self):
-        # In the begin the recipe does't have likes
-        self.asserEqual(self.recipe.number_of_likes(), 0)
-
-        # Add like 
+        self.assertEqual(self.recipe.number_of_likes(), 0)
         self.recipe.likes.add(self.user1)
-        self.assertEqual(self.recipe.numner_of_likes(), 1)
-
-        # Add other likes
-
-             # Added likes
+        self.assertEqual(self.recipe.number_of_likes(), 1)
         self.recipe.likes.add(self.user2)
-        self.assertEqual(self.recipe.numner_of_likes(), 2)
-
-
-    
+        self.assertEqual(self.recipe.number_of_likes(), 2)
--- cook_with_me/settings.py
+++ cook_with_me/settings.py
@@ -15,6 +15,8 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+import sys
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,15 +86,25 @@ WSGI_APPLICATION = 'cook_with_me.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db_test.sqlite3',
+        }
+    }
+elif development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
