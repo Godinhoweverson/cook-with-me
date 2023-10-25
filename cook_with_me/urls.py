003d870f12ac32ce865b37760923ea9dc9a97f1a
Godinhoweverson <>
2023-10-25 08:13:01 +0000

    Add django-allauth for user registratiom - Installed django-allauth and added to INSTALLED_APPS. - Configured redirect to main page after user login.

--- cook_with_me/urls.py
+++ cook_with_me/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('cook_recipe.urls'), name='cook_recipe.urls'),
-   
+    path("accounts/", include("allauth.urls")),
+  
 ]
