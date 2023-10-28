5da78da75937820e9003ed10dc3bdcb8f7be4968
Godinhoweverson <>
2023-10-28 08:47:51 +0000

    Added pagination to the 'recipe_content' view to limit the display to 4 comments per page. - Users can navigate to next or previous pages to view more comments.

--- cook_recipe/views.py
+++ cook_recipe/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
 from .models import Recipe, Comment
 from .forms import CommentForm
+from django.core.paginator import Paginator
 
 
 def home(request):
@@ -14,6 +15,12 @@ def recipe_list(request):
 
 def recipe_content(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
+    comments = recipe.comment_set.all().order_by('-created_on')
+    paginator = Paginator(comments, 4) # Show 4 comments per page
+
+    page = request.GET.get('page')
+    comments = paginator.get_page(page)
+
     form = CommentForm()
 
     if request.method == "POST":
@@ -27,7 +34,7 @@ def recipe_content(request, recipe_id):
                 return redirect('recipe_content', recipe_id=recipe.id)
         else:
             return redirect('account_login')
-    return render(request, 'recipe_content.html', {'form':form, 'recipe':recipe})
+    return render(request, 'recipe_content.html', {'form':form, 'recipe':recipe, 'comments': comments})
 
 
 
