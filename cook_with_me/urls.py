fb190bb424fedc376ebfdc0cac1c3870a938a586
Godinhoweverson <>
2023-10-29 11:46:42 +0000

    Fix navbar issue on home page
    Removed a section from the home page that was causing interference with the navbar's functionality.

--- cook_with_me/urls.py
+++ cook_with_me/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('cook_recipe.urls'), name='cook_recipe.urls'),
     path('accounts/', include("allauth.urls")),
- 
 ]
