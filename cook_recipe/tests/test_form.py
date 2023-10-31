483669643d50348182244fa2da0a92a6f26c3a9a
Godinhoweverson <>
2023-10-31 16:03:13 +0000

    add Password Validation Tests for Signup Form This commit introduces test cases for the 'PasswordSignupForm' to ensure that the password validation logic is functioning correctly.

--- cook_recipe/tests/test_form.py
+++ cook_recipe/tests/test_form.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from cook_recipe.models import Recipe, Comment
-from cook_recipe.forms import CommentForm
+from cook_recipe.forms import CommentForm, PasswordSignupForm
 
 
 class CommentFormTest(TestCase):
@@ -42,3 +42,44 @@ class CommentFormTest(TestCase):
             self.assertEqual(comment.content_body, 'This is another test comment')
         else:
             self.fail('Form is not valid.')
+
+
+class PasswordSignupFormTest(TestCase):
+    def test_valid_password(self):
+        # Create a form with a valid password
+        form = PasswordSignupForm(data={'password1': 'ValidPassword123'})
+        self.assertFalse(form.is_valid())
+    
+
+    def test_short_password(self):
+        # Create a form with a password that is too short
+        form = PasswordSignupForm(data={'password1': 'Short'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('Password must be at least 8 characters long.', form.errors['password1'])
+
+
+    def test_no_uppercase_letter(self):
+        # Create a form with a password that lacks an uppercase letter
+        form = PasswordSignupForm(data={'password1': 'nouppercase123'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('Password must contain at least one uppercase letter.', form.errors['password1'])
+
+
+    def test_no_lowercase_letter(self):
+        # Create a form with a password that lacks an lowercase letter
+        form = PasswordSignupForm(data={'password1': 'NOLOWERCASE123'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('Password must contain at least one lowercase letter.', form.errors['password1'])
+
+
+    def test_no_digit(self):
+        # Create a form with a password that lacks a digit
+        form = PasswordSignupForm(data={'password1': 'NoDigitPassword'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('Password must contain at least one digit.', form.errors['password1'])
+
+
+
+
+    
+
