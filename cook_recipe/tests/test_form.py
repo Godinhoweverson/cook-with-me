643f5a4b568d992dfdefc06ec3d1999cb536815a
Godinhoweverson <>
2023-11-01 11:12:34 +0000

    Added explanatory comments to form and model tests for Recipe and Comment model and form Comment and passwordsignup

--- cook_recipe/tests/test_form.py
+++ cook_recipe/tests/test_form.py
@@ -1,49 +1,63 @@
+#Import necessary modules
 from django.test import TestCase
 from cook_recipe.models import Recipe, Comment
-from cook_recipe.forms import CommentForm, PasswordSignupForm
+from cook_recipe.forms import CommentForm
 
-
+# Define a test class for CommentForm
 class CommentFormTest(TestCase):
 
-    
+    # Test the form's meta class
     def test_form_meta_class(self):
         form = CommentForm()
+        # Check if the form's model matches the Comment model
         self.assertEqual(form.Meta.model, Comment)
+        # Check if the form's fields include only 'content_body'
         self.assertEqual(list(form.Meta.fields), ['content_body'])
     
-
+    # Test a valid form submission
     def test_valid_form(self):
+        # Create a Recipe instance
         recipe = Recipe.objects.create(title='Test Recipe')
+        # Define form data with content_bod and associated recipe
         data = {
             'content_body': 'This is a test comment',
             'recipe': recipe.id 
         }
 
         form = CommentForm(data=data)
+        # Check if the form is valid
         self.assertTrue(form.is_valid())
 
-
+    # Test an invalid form submission (missing content_body)
     def test_invalid_form(self):
         data = {'content_body': ''}
         form = CommentForm(data=data)
+        # Check if the form is not valid
         self.assertFalse(form.is_valid())
 
-
+    # Test saving the form data
     def test_form_save(self):
+        # Create a Recipe instance
         recipe = Recipe.objects.create(title='Test Recipe')
+        # Define form data with content_body
         data = {
             'content_body': 'This is another test comment'
         }
         form = CommentForm(data=data)
         if form.is_valid():
+            # Save the comment object without committing to the database
             comment = form.save(commit=False)
             comment.recipe = recipe
             comment.save()
+            # Check if the content_body is correctly saved
             self.assertEqual(comment.content_body, 'This is another test comment')
         else:
             self.fail('Form is not valid.')
 
-
+    
+# Tests for check password validation rules.
+# It tests valid passwords, short passwords, missign uppercase letters, missing lowercase letters, and missing digits.
+# The tests check if the form correctly identifies these issues and reports appropriate error messages.
 class PasswordSignupFormTest(TestCase):
     def test_valid_password(self):
         # Create a form with a valid password
--- cook_recipe/tests/test_models.py
+++ cook_recipe/tests/test_models.py
@@ -3,25 +3,35 @@ from django.contrib.auth.models import User
 from cook_recipe.models import Recipe, Comment
 
 
+# Define a test class for the Recipe model
 class RecipeModelTest(TestCase):
 
+
     def setUp(self):  
+        # Create two user instances
         self.user1 = User.objects.create_user(username='testuser1', password='testpass123')
         self.user2 = User.objects.create_user(username='testuser2', password='testpass456')
 
-        
+    
+        # Create a Recipe instance
         self.recipe = Recipe.objects.create(
             title='Test Recipe',
             content='This is a test content for the recipe.'
         )
 
+
+    # Test wheter the recipe is created correctly
     def test_recipe_creation(self):
         self.assertEqual(self.recipe.title, 'Test Recipe')
         self.assertEqual(self.recipe.content, 'This is a test content for the recipe.') 
 
+
+    # Test the string representation of the recipe
     def test_recipe_str_representation(self):
         self.assertEqual(str(self.recipe), 'Test Recipe')
 
+
+    # Test the number of likes for the recipe
     def test_number_of_likes(self):
         self.assertEqual(self.recipe.number_of_likes(), 0)
         self.recipe.likes.add(self.user1)
@@ -30,32 +40,44 @@ class RecipeModelTest(TestCase):
         self.assertEqual(self.recipe.number_of_likes(), 2)
 
 
+# Define a test class for the Comment model
 class CommentModelTest(TestCase):
 
 
     def setUp(self):
+        # Create a user instance
         self.user = User.objects.create_user(
             username = 'testuser',
             password = 'testpass123'
         )
 
+
+        # Create a Recipe instance 
         self.recipe = Recipe.objects.create(
             title = 'Test Recipe'
         )
 
+
+        # Create a Comment instance
         self.comment = Comment.objects.create(
             recipe = self.recipe,
             user = self.user,
             content_body = 'This is a test comment.',
         )
 
+
+    # Test wheter the comment is create correctly
     def test_comment_creation(self):
         self.assertIsInstance(self.comment, Comment)
         self.assertEqual(self.comment.content_body, 'This is a test comment.')
 
+
+   # Test the string representation of the comment
     def test_comment_str(self):
         self.assertEqual(str(self.comment), 'Comment This is a test comment. by testuser')
 
+
+    # Test a comment without a user
     def test_comment_without_user(self):
         comment_without_user = Comment.objects.create(
             recipe = self.recipe,
