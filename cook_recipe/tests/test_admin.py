5fc3953be941d01eecd290fb0d12269e797f6300
Godinhoweverson <>
2023-11-01 20:44:26 +0000

    Added admin tests for Recipe and Comments. This commit adds tests cases to verify the functionality of the Django admin interface for the Recipe and Comments models in the 'cook_recipe'app. The tests include checking the admin page access, comment approval action, and setting up a user for testing.

---
+++ cook_recipe/tests/test_admin.py
@@ -0,0 +1,67 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from cook_recipe.models import Recipe, Comment
+
+class AdminTests(TestCase):
+    def setUp(self):
+        # Create a user for testing
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+    def test_recipe_admin(self):
+        recipe = Recipe.objects.create(
+            title='Test Recipe',
+            created_date='2023-01-01',
+            content='Test content',
+            content_method='Test content method'
+        )
+
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get('/admin/cook-recipe/recipe/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_comment_admin(self):
+        comment = Comment.objects.create(
+            recipe=Recipe.objects.create(
+                title='Test Recipe',
+                created_date='2023-01-01',
+                content='Test content',
+                content_method='Test content method'
+            ),
+            user=self.user,
+            created_on='2023-01-01',
+            content_body='Test comment body',
+            comment_approved=False
+        )
+
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get('/admin/cook_recipe/comment/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_comment_approve_action(self):
+        comment = Comment.objects.create(
+            recipe=Recipe.objects.create(
+                title='Test Recipe',
+                created_date='2023-01-01',
+                content='Test content',
+                content_method='Test content method'
+            ),
+            user=self.user,
+            created_on='2023-01-01',
+            content_body='Test comment body',
+            comment_approved=False
+        )
+
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post('/admin/cook-with-me/comment/', {
+            '_selected_action': [str(comment.id)],
+            'action': 'approve_comments',
+            'post': 'yes',
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Comment.objects.get(pk=comment.pk).comment_approved, False)
