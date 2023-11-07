2acc0cdcf6a870537454d1d9ce1b334fdf72aefa
Godinhoweverson <>
2023-11-07 11:13:22 +0000

    Fix image path for background image in CSS - updated to the correct path

--- cook_with_me/settings.py
+++ cook_with_me/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cook-with-me-0c33f788a701.herokuapp.com', 'localhost', '8000-godinhoweverson-cook-wit-ytgsz655q0.us2.codeanyapp.com']
 
