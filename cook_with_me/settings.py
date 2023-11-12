46e5a5398930212c11ad14d15e648bab349d8157
Godinhoweverson <>
2023-11-12 22:18:47 +0000

    Debug = false

--- cook_with_me/settings.py
+++ cook_with_me/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.environ.get("IS_PRODUCTION") == 'YES' else True
 
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
